f912222c9efd6e2b21c9f0c6ebdd0b461a39eaa5
kumdan job <redeks123456@gmail.com>
2023-11-17 22:35:52 +0100

    m

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sites.db"
 db = SQLAlchemy(app)
 # db.init_app(app)
 

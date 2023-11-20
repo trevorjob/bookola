0842fd326d1655e15a9e96959e316106d9d7802e
Adeosun Adeola <adeolaesther761@gmail.com>
2023-11-20 14:34:24 +0100

    changes

--- models/author.py
+++ models/author.py
@@ -8,25 +8,17 @@ from models import db
 from models.base import *
 from models.reviews import *
 
+
 class Author(Base):
     """
-    The Author class represents book information in the 'books' table.
+    The Author class represents book information in the 'authors' table.
 
     Attributes:
         __tablename__ (str): The name of the database table.
-        title (str): The title of the book. It is a required field.
-        author (str): The author(s) of the book. It is a required field.
-        publiication_date (date): The date when the book was published.
-                It can be None if the publication date is unknown.
-        genre (str): The genre or category of the book.
-                It can be None if the genre is not specified.
-        language (str): The language in which the book is written.
-                It can be None if the language is not specified.
-        description (str): A brief summary or description of the book.
-                It can be None if a summary is not available.
-        cover_image_url (str): The URL of the book's cover image.
-                It can be None if the cover image is not available.
-        rating (Integer): Rating.
+
     """
-    __tablename__ = 'author'
-    
+    __tablename__ = 'authors'
+    user_id = db.Column(db.String(60), db.ForeignKey('users.id'),
+                        nullable=False)
+    book_id = db.Column(db.String(60), db.ForeignKey('books.id'),
+                        nullable=False)

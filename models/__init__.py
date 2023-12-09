8e2fc38ce32ebb8a6efce5feb0ac59befd73b444
kumdan job <redeks123456@gmail.com>
2023-12-09 02:57:27 +0100

    added genres page

--- models/__init__.py
+++ models/__init__.py
@@ -17,8 +17,8 @@ app.config["SECRET_KEY"] = os.environ.get("FLASK_SECRET_KEY")
 # Mail Configuration
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
-app.config["MAIL_USERNAME"] = 'doe70097@gmail.com'
-app.config["MAIL_PASSWORD"] = 'awhw jjwx ktbr lyls'
+app.config["MAIL_USERNAME"] = "doe70097@gmail.com"
+app.config["MAIL_PASSWORD"] = "awhw jjwx ktbr lyls"
 app.config["MAIL_USE_TLS"] = False
 app.config["MAIL_USE_SSL"] = True
 # Enable mail debugging for development
@@ -30,8 +30,8 @@ db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 socketio = SocketIO(app)
 stripe.api_key = os.environ.get("STRIPE_SECRET_KEY")
-# YOUR_DOMAIN = "https://bookola.onrender.com"
-YOUR_DOMAIN = "http://127.0.0.1:5000"
+YOUR_DOMAIN = "https://bookola.onrender.com"
+# YOUR_DOMAIN = "http://127.0.0.1:5000"
 
 
 if __name__ == "__main__":
